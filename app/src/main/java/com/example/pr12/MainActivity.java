b55a3613af8537911b881959afdce929d3a6346d
Hat <cool97438@gmail.com>
2024-11-22 23:56:37 +0500

    Разработанная функция AlertDialog. Kurlishuk

--- app/src/main/java/com/example/pr12/MainActivity.java
+++ app/src/main/java/com/example/pr12/MainActivity.java
@@ -8,6 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import androidx.appcompat.app.AlertDialog;
+
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.EditText;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +27,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void AlertDialog(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("ОК", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {dialog.cancel();}
+                });
+
+        AlertDialog alter = builder.create();
+        alter.show();
+    }
+
+    public EditText fio;
+    public EditText phone;
+    public EditText adress;
+    public void OnArrange(View view)
+    {
+        fio = findViewById(R.id.fio);
+        phone = findViewById(R.id.phone);
+        adress = findViewById(R.id.adress);
+
+        if(fio.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите Фамилию, Имя и Отчество.");
+        else if(phone.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите номер телефона.");
+        else if(adress.getText().length() == 0) AlertDialog("Уведомление", "Пожалуйста, укажите адрес доставки.");
+        else {
+            AlertDialog("Уведомление", "Заказ оформлен.");
+        }
     }
 }
